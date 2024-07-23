9107e395b5877b0d3707221a8cb7531d9bb0de8f
ivanmaxibt <107760930+ivanmaxibt@users.noreply.github.com>
2024-07-22 23:45:47 -0300

    Update fecha.js

--- js/fecha.js
+++ js/fecha.js
@@ -1,5 +1,6 @@
 function updateClock() {
     const now = new Date();
+    const dayOfWeek = now.getDay();
     const year = now.getFullYear();
     const month = (now.getMonth() + 1).toString().padStart(2, '0');
     const day = now.getDate().toString().padStart(2, '0');
