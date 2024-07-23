6236ab4dfb2770d1ea9340518b5b8e8e09b62b99
ivanmaxibt <107760930+ivanmaxibt@users.noreply.github.com>
2024-07-22 23:47:58 -0300

    Update saludo.js

--- js/saludo.js
+++ js/saludo.js
@@ -5,5 +5,11 @@ let form = document.getElementById('form');
               localStorage.setItem('nombre', nombre);
               let nombreGuardado = localStorage.getItem('nombre');
               let saludo = document.getElementById('saludo');
-              saludo.textContent = `Bienvenido: ${nombreGuardado}. Espero que se encuentre muy bien. A continuación podrá consultar el catálogo:`;
+              const now = new Date();
+              const dayOfWeek = now.getDay();
+              if (dayOfWeek === 2) {
+              document.getElementById('saludo').textContent = `¡Bienvenido: ${nombreGuardado}! Hoy es martes, ¡aprovecha nuestro 20% de descuento!`;
+              } else {
+              document.getElementById('saludo').textContent = `¡Bienvenido: ${nombreGuardado}! Espero que se encuentre muy bien. A continuación podrá consultar el catálogo:`;
+              };
             });
